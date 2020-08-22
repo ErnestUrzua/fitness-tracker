5cbbb53b4027d6ba474e2ff1bf496889b3cdf6c8
LoneStar <ernieurzua@gmail.com>
2020-08-21 20:26:46 -0700

    added server js for mongoose and express

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+//required files express for server and mongoose for our db models
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+//our port we to listen to locally
+const PORT = process.env.PORT || 3000;
+
+//our server app
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//allow our public forlder to be statically served
+app.use(express.static("public"));
+
+//our mongo server 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+// our required routes 
+// app.use(require("./routes/api.js"));
+// app.use(require("./routes/html.js"));
+
+
+app.listen(PORT, () => {
+  console.log("App running on port " + PORT);
+});
