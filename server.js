b732be10d819ce9ce9cdf4f0b429700a7e9c3d23
LoneStar <ernieurzua@gmail.com>
2020-08-22 11:22:33 -0700

    added routes for our express app to use

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 
 // our required routes 
-// app.use(require("./routes/api.js"));
-// app.use(require("./routes/html.js"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 
 app.listen(PORT, () => {
